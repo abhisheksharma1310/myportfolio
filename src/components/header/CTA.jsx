096e5cb56ec71e515603663f6b39671462aa3c58
abhisheksharma <abhisheksharma131098@gmail.com>
2023-10-26 15:36:51 +0530

    projects and resume url modified

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import CV from '../../assets/Abhishek_Sharma_Web_Developer_CV.pdf';
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} target='_blank' rel="noreferrer" download className='btn' title='Download Resume'>Download Resume</a>
+        <a href="https://drive.google.com/drive/folders/1HHx54RtahMfw7LDjKWeXnD6NHBorWR7k?usp=sharing" target='_blank' rel="noreferrer" download className='btn' title='View Resume'>View Resume</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
 
